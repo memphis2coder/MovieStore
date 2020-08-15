1b25dda7f07215e794a76b0019a0b680e9f0ea39
Antonio <memphis2coder@gmail.com>
2020-08-14 22:44:16 -0400

    working on main app

--- src/App.js
+++ src/App.js
@@ -4,30 +4,17 @@ import Navigation from './components/navbar/Navigation';
 import Cards from './components/cards/Cards';
 import Api from './Api';
 import axios from 'axios'
+import Search from './components/search/search';
 
 const apiKey = process.env.REACT_APP_API_KEY
 
 function App() {
-
-  const [post, setPost] = useState=({});
-
-  useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${post}&page=1&include_adult=false`)
-        .then(res => {
-            setPost(res.data.results[0])
-            console.log(res.data.results)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-}, [])
-
+  
   return (
     <div className="App">
         <Navigation />
           <div className="container">
-            <Api />
-            <Cards />
+            <Search />
           </div>
     </div>
     
